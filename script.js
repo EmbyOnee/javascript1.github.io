5df0b2d22e9ecfa6896af396395e78280d43c085
EmbyOnee <105348885+EmbyOnee@users.noreply.github.com>
2022-06-04 19:56:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+var userName1 = prompt('Введите ваше имя');
+var userAge1 = prompt('Введите ваш возраст');
+var userName2 = prompt('Введите ваше имя');
+var userAge2 = prompt('Введите ваш возраст');
+var userName3 = prompt('Введите ваше имя');
+var userAge3 = prompt('Введите ваш возраст');
+var userName4 = prompt('Введите ваше имя');
+var userAge4 = prompt('Введите ваш возраст');
+var userName5 = prompt('Введите ваше имя');
+var userAge5 = prompt('Введите ваш возраст');
+var userName6 = prompt('Введите ваше имя');
+var userAge6 = prompt('Введите ваш возраст');
+var userName7 = prompt('Введите ваше имя');
+var userAge7 = prompt('Введите ваш возраст');
+var userName8 = prompt('Введите ваше имя');
+var userAge8 = prompt('Введите ваш возраст');
+var userName9 = prompt('Введите ваше имя');
+var userAge9 = prompt('Введите ваш возраст');
+var userName10 = prompt('Введите ваше имя');
+var userAge10 = prompt('Введите ваш возраст');
+
+
+let user1 = {
+    name: userName1 ,
+    age: userAge1
+}
+let user2 = {
+    name: userName2,
+    age: userAge2
+}
+let user3 = {
+    name: userName3,
+    age: userAge3
+}
+let user4 = {
+    name: userName4 ,
+    age: userAge4
+}
+let user5 = {
+    name: userName5 ,
+    age: userAge5
+}
+let user6 = {
+    name: userName6 ,
+    age: userAge6
+}
+let user7 = {
+    name: userName7 ,
+    age: userAge7
+}
+let user8 = {
+    name: userName8,
+    age: userAge8
+}
+let user9 = {
+    name: userName9 ,
+    age: userAge9
+}
+let user10 = {
+    name: userName10 ,
+    age: userAge10
+}
+
+console.log('Ваше имя ' + user1.name + ' Ваш возраст ' + user1.age);
+console.log('Ваше имя ' + user2.name + ' Ваш возраст ' + user2.age);
+console.log('Ваше имя ' + user3.name + ' Ваш возраст ' + user3.age);
+console.log('Ваше имя ' + user4.name + ' Ваш возраст ' + user4.age);
+console.log('Ваше имя ' + user5.name + ' Ваш возраст ' + user5.age);
+console.log('Ваше имя ' + user6.name + ' Ваш возраст ' + user6.age);
+console.log('Ваше имя ' + user7.name + ' Ваш возраст ' + user7.age);
+console.log('Ваше имя ' + user8.name + ' Ваш возраст ' + user8.age);
+console.log('Ваше имя ' + user9.name + ' Ваш возраст ' + user9.age);
+console.log('Ваше имя ' + user10.name + ' Ваш возраст ' + user10.age);
+
+let allUsers = {
+    user1: user1,
+    user2: user2,
+    user3: user3,
+    user4: user4,
+    user5: user5,
+    user6: user6,
+    user7: user7,
+    user8: user8,
+    user9: user9,
+    user10: user10
+}
+
+console.log(allUsers);
+
+
+
